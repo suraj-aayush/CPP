568731c389388a366412d11e871b769a3ff0f354
Aayush Kumar <105626112+suraj-aayush@users.noreply.github.com>
2023-05-31 10:35:51 +0530

    Create DFS.cpp

---
+++ 1_IMP_ALGOS/DFS.cpp
@@ -0,0 +1,42 @@
+void dfs(int node, unordered_map<int,list<int>> &adj, unordered_map<int,bool> &visited, vector<int> &component )
+{
+    component.push_back(node);
+    visited[node] = true;
+    
+    for(auto i:adj[node])
+    {
+        if( !visited[i] )
+        {
+            dfs(i,adj,visited,component);
+        }
+    }
+}
+
+vector<vector<int>> depthFirstSearch(int V, int E, vector<vector<int>> &edges)
+{ 
+    vector<vector<int>> ans;
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<edges.size();i++)
+    {
+        int u = edges[i][0];
+        int v = edges[i][1];
+        
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    unordered_map<int,bool> visited;
+    for(int i=0;i<V;i++)
+    {
+        if( !visited[i] )
+        {
+            vector<int> component;
+            dfs(i,adj,visited,component);
+            
+            ans.push_back(component);
+        }
+    }
+
+    return ans;
+    
+}
