e3b98ea3abd0881c72390d5f361c7c4ad568c0b0
Aayush Kumar <aayushagrawal296@gmail.com>
2024-02-11 16:43:52 +0530

    names

---
+++ 06_Recursion/String Recursion/PalindromeStringREC.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool pali(string str, int l, int r)
+{
+    if(l>r)
+    return true;
+
+    if(str[l] != str[r])
+    return false;
+
+    else
+    return pali(str,l+1,r-1);
+
+}
+
+int main(){
+    string str = "abcdcba";
+    bool ans = pali(str,0,str.size()-1);
+    if(ans)
+    cout<<" PALI ";
+    else
+    cout<<" NOT PALI ";
+    
+return 0;
+}
